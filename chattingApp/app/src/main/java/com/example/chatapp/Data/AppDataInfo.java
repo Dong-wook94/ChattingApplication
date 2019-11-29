5d45c93c49c2fa3fb221258a574d84c30dc88ece
Dong-wook94 <gygacpu@naver.com>
2019-11-29 21:37:39 +0900

    디렉토리 정리

---
+++ chattingApp/app/src/main/java/com/example/chatapp/Data/AppDataInfo.java
@@ -0,0 +1,10 @@
+package com.example.chatapp.Data;
+
+public class AppDataInfo {
+    public static final class Login {
+        public static final String key = "logininfo";
+        public static final String checkbox = "checkbox";
+        public static final String userID = "userID";
+        public static final String userPwd = "userPwd";
+    }
+}
